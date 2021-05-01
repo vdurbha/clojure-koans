921aa7f795e887c93158bab046e1fca087f46b27
vdurbha <viswanath.durbha@gmail.com>
2021-05-01 16:00:18 +0530

    Completed Recursion

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,29 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [origcoll coll
+         revcoll '()]
+    (if (empty? origcoll)
+      revcoll
+      (recur (rest origcoll) (cons (first origcoll) revcoll)))))
 
 (defn factorial [n]
-  __)
+  (loop [curval n
+         res n]
+    (if (= curval 1)
+      res
+      (recur (- curval 1) (* res (- curval 1))))))
 
 (meditations
   "Recursion ends with a base case"
